6e53ee7e5e86e9f3d73788290a3b07cd955b02d1
step <step@iic.md>
2021-12-02 21:17:31 +0200

    Added Pages to Movie app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Link, Route, Switch } from "react-router-dom";
+import Avengers from "./components/Avengers";
+import Home from "./components/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/avengers">Avengers</Link>
+        </li>
+      </nav>
+
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/avengers">
+          <Avengers />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Avengers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Avengers = () => {
+  return (
+    <div>
+      <h1>Avengers</h1>
+    </div>
+  );
+};
+
+export default Avengers;
